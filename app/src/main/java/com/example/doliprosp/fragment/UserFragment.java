4a794440cadf33e1544720997246828095fd9901
antoninVeyre12 <antonin.veyre@iut-rodez.fr>
2024-10-18 14:04:03 +0200

    implémentation des différentes classes Java + implémentation de l'interface de Application

--- app/src/main/java/com/example/doliprosp/fragment/UserFragment.java
+++ app/src/main/java/com/example/doliprosp/fragment/UserFragment.java
@@ -1,14 +1,17 @@
-package com.example.doliprosp;
+package com.example.doliprosp.fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
-public class ProspectFragment extends Fragment {
+import com.example.doliprosp.R;
+
+public class UserFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_prospect, container, false);
+                             Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_user, container, false);
     }
 }
