75879aa4c491f3f0465480b635a339cbb2127bde
alixB31 <131276536+alixB31@users.noreply.github.com>
2024-10-18 15:33:20 +0200

    Update ShowAdapter

--- app/src/main/java/com/example/doliprosp/adapter/ShowAdapter.java
+++ app/src/main/java/com/example/doliprosp/adapter/ShowAdapter.java
@@ -27,13 +27,14 @@ public class ShowAdapter extends RecyclerView.Adapter<ShowAdapter.MyViewHolder> 
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_show, parent, false);
         return new MyViewHolder(view);
+
     }
 
     // Remplit le ViewHolder avec les données
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         Show show = showList.get(position);
-
+        holder.show_name.setText(show.getName());
     }
 
     @Override
@@ -43,9 +44,10 @@ public class ShowAdapter extends RecyclerView.Adapter<ShowAdapter.MyViewHolder> 
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
 
+        public TextView show_name;
         public MyViewHolder(View itemView) {
             super(itemView);
-
+            show_name = itemView.findViewById(R.id.show_name);
         }
     }
 }
