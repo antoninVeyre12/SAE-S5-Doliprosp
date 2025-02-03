50febec0551ea5067e5d31238384460c1cddd2bf
antoninVeyre12 <antonin.veyre@iut-rodez.fr>
2025-02-03 11:14:31 +0100

    fin de la revue de code de la connexion

--- app/src/main/java/com/example/doliprosp/fragment/SalonFragment.java
+++ app/src/main/java/com/example/doliprosp/fragment/SalonFragment.java
@@ -134,7 +134,6 @@ public class SalonFragment extends Fragment implements MyShowAdapter.OnItemClick
      */
     private void rechercheSalons(String recherche){
         Utilisateur utilisateur = utilisateurViewModel.getUtilisateur();
-        Log.d("urlll", utilisateur.getUrl());
         chargement.setVisibility(View.VISIBLE);
 
         salonService.getSalonsEnregistres(getContext(),recherche, utilisateur, new Outils.APIResponseCallbackArrayTest() {
