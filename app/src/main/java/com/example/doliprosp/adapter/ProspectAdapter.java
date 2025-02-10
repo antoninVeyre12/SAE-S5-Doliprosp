c9f8c2082eb23c130996843ed021039764824c0c
Bratoun <rayan.ibrahime@iut-rodez.fr>
2025-02-10 10:45:43 +0100

    Update ProspectAdapter.java

--- app/src/main/java/com/example/doliprosp/adapter/ProspectAdapter.java
+++ app/src/main/java/com/example/doliprosp/adapter/ProspectAdapter.java
@@ -11,15 +11,15 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.doliprosp.Modele.Prospect;
 import com.example.doliprosp.R;
 import com.example.doliprosp.viewModel.MesProspectViewModel;
 
 import java.io.Serializable;
 import java.util.List;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
 
 // La classe ProspectAdapter est un adaptateur pour lier une liste de prospects à un RecyclerView.
 // Elle implémente Serializable pour la sérialisation de l'adaptateur.
@@ -196,46 +196,40 @@ public class ProspectAdapter extends RecyclerView.Adapter<ProspectAdapter.MyView
                 if (nouveauNomPrenom.length() <= 2) {
                     erreurProspect.setText(R.string.erreur_nom_prospect_longueur);
                     erreurProspect.setVisibility(View.VISIBLE);
-                    return;
                 }
                 // Vérification du mail
                 else if (!nouveauMail.matches(REGEX_MAIl)) {
                     erreurProspect.setText(R.string.erreur_mail_prospect);
                     erreurProspect.setVisibility(View.VISIBLE);
-                    return;
                 }
                 // Vérification du téléphone
                 else if (!nouveauTel.startsWith("0")) {
                     erreurProspect.setText(R.string.erreur_tel_prospect_zero);
                     erreurProspect.setVisibility(View.VISIBLE);
-                    return;
                 } else if (!nouveauTel.matches(REGEX_TEL)) {
                     erreurProspect.setText(R.string.erreur_tel_prospect);
                     erreurProspect.setVisibility(View.VISIBLE);
-                    return;
                 }
                 // Vérification de l'adresse
                 else if (nouvelleAdresse.isEmpty()) {
                     erreurProspect.setText(R.string.erreur_adresse_prospect_vide);
                     erreurProspect.setVisibility(View.VISIBLE);
-                    return;
                 }
                 // Vérification de la ville
                 else if (nouvelleVille.isEmpty()) {
                     erreurProspect.setText(R.string.erreur_ville_prospect_vide);
                     erreurProspect.setVisibility(View.VISIBLE);
-                    return;
                 }
                 // Vérification du code postal
                 else if (nouveauCodePostal.length() < 5) {
                     erreurProspect.setText(R.string.erreur_codePostal_prospect);
                     erreurProspect.setVisibility(View.VISIBLE);
-                    return;
+                } else {
+                    erreurProspect.setTextColor(Color.RED);
+                    erreurProspect.setVisibility(View.GONE);
+                    dialog.dismiss();
+                    onItemClickListener.OnModifyClick(position, nouveauNomPrenom, nouveauMail, nouveauTel, nouvelleAdresse, nouvelleVille, nouveauCodePostal);
                 }
-                erreurProspect.setTextColor(Color.RED);
-                erreurProspect.setVisibility(View.GONE);
-                dialog.dismiss();
-                onItemClickListener.OnModifyClick(position, nouveauNomPrenom, nouveauMail, nouveauTel, nouvelleAdresse, nouvelleVille, nouveauCodePostal);
             });
         }
     }
