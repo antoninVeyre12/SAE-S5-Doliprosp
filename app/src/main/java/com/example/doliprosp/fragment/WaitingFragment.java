16e0528d0f57658efd4ae850276d946d789fc14b
alixB31 <131276536+alixB31@users.noreply.github.com>
2025-02-12 13:40:22 +0100

    Update WaitingFragment.java

--- app/src/main/java/com/example/doliprosp/fragment/WaitingFragment.java
+++ app/src/main/java/com/example/doliprosp/fragment/WaitingFragment.java
@@ -10,13 +10,6 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.doliprosp.Interface.IProjetService;
 import com.example.doliprosp.Interface.IProspectService;
 import com.example.doliprosp.Interface.ISalonService;
@@ -41,6 +34,13 @@ import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 /**
  * Fragment affichant la liste des salons en attente.
@@ -207,7 +207,8 @@ public class WaitingFragment extends Fragment {
                                     prospectService.getProspectDUnSalon(mesProspectViewModel.getProspectListe(), salonAEnvoyer.getNom());
                             int idSalon = Integer.parseInt(response);
 
-                            salonsViewModel.removeSalon(salonAEnvoyer, getContext());
+//                            salonsViewModel.removeSalon(salonAEnvoyer,
+//                                    getContext());
                             adapterSalons.notifyDataSetChanged();
                             envoyerProspects(prospectSelectionnes, idSalon,
                                     salonAEnvoyer);
