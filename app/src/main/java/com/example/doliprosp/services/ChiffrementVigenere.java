638ccf3967f15674a54e673739bdb99c01c7f0f1
antoninVeyre12 <antonin.veyre@iut-rodez.fr>
2025-03-24 10:37:24 +0100

    update du dictionnaire de chiffrement vigenere

--- app/src/main/java/com/example/doliprosp/services/ChiffrementVigenere.java
+++ app/src/main/java/com/example/doliprosp/services/ChiffrementVigenere.java
@@ -2,7 +2,7 @@ package com.example.doliprosp.services;
 
 public class ChiffrementVigenere {
 
-    private static char[] alphabet = {'a','A', 'b','B', 'c','C', 'd','D', 'e','E', 'f','F', 'g','G',
+    private static char[] alphabet = {'a','à','â','A', 'b','B', 'c','C', 'd','D', 'e','E','é','è', 'f','F', 'g','G',
             'h','H', 'i','I','j','J', 'k','K', 'l','L', 'm','M', 'n','N', 'o','O', 'p','P', 'q','Q',
             'r','R', 's','S', 't','T', 'u','U', 'v','V', 'w','W', 'x','X', 'y','Y','z','Z', '0', '1',
             '2', '3', '4', '5', '6', '7', '8', '9', '-'};
